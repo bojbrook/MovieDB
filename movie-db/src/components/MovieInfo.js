77335d7930f103ae8216a8f36aaf99e296a4a1b0
Bowen Brooks <39347269+bojbrook@users.noreply.github.com>
2022-07-05 14:07:17 -0700

    added callbacks for the movie info

--- movie-db/src/components/MovieInfo.js
+++ movie-db/src/components/MovieInfo.js
@@ -1,30 +1,35 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { MOVIE_INFO_URL, MOVIE_API } from '../utils/constants';
+import { MOVIE_API, MOVIE_INFO_URL } from '../utils/constants';
 
-const MovieInfo = (props) => {
+const MovieInfo = () => {
   const [movie, setMovie] = useState({});
   let params = useParams();
   const movieId = parseInt(params.id, 10);
 
-  const getMovieDetail = async (url) => {
+
+  const getMovieDetail = useCallback(async (url) =>{
     const results = await fetch(url);
     const data = await results.json();
 
-    const movieObj = {
+    return {
       title: data.title,
       description: data.overview,
-    };
-    setMovie(movieObj);
-  };
+      user_review: data.vote_average,
+    }
+  },[])
 
   useEffect(() => {
-    const url = `${MOVIE_INFO_URL}/${movieId}?api_key=${MOVIE_API}&language=en-US`;
-    getMovieDetail(url);
-  });
+      const url = `${MOVIE_INFO_URL}/${movieId}?api_key=${MOVIE_API}&language=en-US`;
+      const image_url = `${MOVIE_INFO_URL}/${movieId}/images?api_key=${MOVIE_API}&language=en-US`;
+      console.log(image_url);
+      getMovieDetail(url).then(r => setMovie(r));
+
+    }, [getMovieDetail, movieId]);
   return (
     <>
       <h1>{movie.title}</h1>
+      <p>{movie.user_review}</p>
       <p>{movie.description}</p>
     </>
   );
